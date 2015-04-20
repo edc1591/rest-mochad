9d80a79630180c22b0ec45c335e0d3822399ed03
Evan Coleman <evan@evancoleman.net>
2015-04-19 21:45:34 -0400

    Send protocol in request

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var bodyParser = require('body-parser');
 var app = express();
 var queue = require('queue-async')(1);
 
-// Force certain devices to be controlled over RF instead of PL
-var rfDevices = ['C1', 'E1'];
-
 app.use(bodyParser.urlencoded({
   extended: true
 })); 
@@ -24,7 +21,7 @@ function queueCommand(command) {
 var client = net.connect(1099, function(){
   app.post('/x10/power', function (req, res) {
     var location = req.param('house_code') + req.param('device_id');
-    var method = (rfDevices.indexOf(location) > -1) ? 'rf' : 'pl';
+    var method = req.param('protocol'); // rf or pl
     console.log('Queuing Power Event');
     var command = req.param('value') == 'true' ? 'on' : 'off';
     var ncCommand = method + ' ' + location + ' ' + command + '\n';
@@ -34,7 +31,7 @@ var client = net.connect(1099, function(){
 
   app.post('/x10/brightness', function (req, res) {
     var location = req.param('house_code') + req.param('device_id');
-    if (rfDevices.indexOf(location) == -1) {
+    if (req.param('protocol') == "pl") {
       console.log('Queuing Brightness Event');
       var command = 'xdim';//req.param('value') == 'true' ? 'on' : 'off';
       var val = Math.round((parseInt(req.param('value')) / 100) * 70);
